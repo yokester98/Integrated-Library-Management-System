2da842b6e734cf2f72081bda911faeedc82770c8
yokester98 <39672956+yokester98@users.noreply.github.com>
2021-03-17 00:14:21 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,42 @@
+# import dependencies
+from flask import Flask, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return render_template('Home.html')
+
+@app.route("/Home.html")
+def main2():
+    return render_template('Home.html')
+
+@app.route("/Search.html")
+def search():
+    return render_template('Search.html')
+
+@app.route("/Manage.html")
+def manage():
+    headings = ("BookID", "Status", "Due/Available Date", "Action")
+    data = (("123", "Borrowed", "21/01/2021", "Return/Extend"), ("456", "Reserved", "16/03/2021", "Convert/Cancel"))
+    return render_template('Manage.html', data = data)
+
+@app.route("/Payment.html")
+def payment():
+    return render_template('Payment.html')
+
+@app.route("/Admin.html")
+def admin():
+    return render_template('Admin.html')
+
+@app.route("/success")
+def success():
+    return render_template('Success.html')
+
+@app.route("/fail")
+def fail():
+    return render_template('Fail.html')
+
+# final line
+if __name__ == "__main__":
+    app.run()
