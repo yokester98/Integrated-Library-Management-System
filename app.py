81daa56b3c3cc7a8994eab80bed2e89eb18ba618
radellng <77708166+radellng@users.noreply.github.com>
2021-03-18 00:49:56 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 # import dependencies
 from flask import Flask, render_template, redirect, url_for
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+mongodb = MongoClient('localhost', 27017)
+db = mongodb.bookcollection
+collection = db.book
 
 @app.route("/")
 def main():
@@ -11,9 +16,20 @@ def main():
 def main2():
     return render_template('Home.html')
 
+@app.route('/simpleSearch',methods=['POST'])
+def simpleSearch():
+    _keyword = request.form['u-border-1 u-border-grey-30 u-input u-input-rectangle u-white']
+    return collection.find({title : _keyword})
+
 @app.route("/Search.html")
 def search():
-    return render_template('Search.html')
+    headings = ("BookID", "Title", "ISBN", "Return Date")
+    data = (("123", "lala", "123456", "NULL"), ("456", "lalala", "123456789", "17/03/2021"))
+    return render_template('Search.html', data = data)
+
+@app.route("/Result.html")
+def result():
+    return render_template('Result.html')
 
 @app.route("/Manage.html")
 def manage():
